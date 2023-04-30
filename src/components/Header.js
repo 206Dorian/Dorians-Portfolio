034c1c93beda1579d0472d1d03fedd45d9a0293b
Dorian Birch <206dorian@gmail.com>
2023-04-30 15:32:54 -0500

    testing push

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1> 🦾 DIDACTIC LIFT 🦾 </h1>
+
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Workout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { css } from '@emotion/react'
+
+// const Button = styled.button`
+//   color: turquoise;
+// `
+
+// render(<btn>This my button component.</btn>)
+
+const Workout = () => {
+  return (
+    <>
+
+      
+      <h2>MY WORKOUTS
+
+<li> <a> ARMS </a></li>
+<br />
+
+<li> <a> BACK </a></li>
+<br />
+
+<li><a> CHEST </a></li>
+</h2>
+
+
+
+      </>
+  )
+}
+
+export default Workout
