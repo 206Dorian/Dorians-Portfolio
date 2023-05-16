acbc261f90ea1ac9eac2c714b50eb0af04e9632c
Dorian Birch <206dorian@gmail.com>
2023-05-15 20:31:00 -0500

    debugging Loading page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Loading from './Loading';
+import Loading from './components/Loading';
 import logo from './logo.svg';
 import './App.css';
 
