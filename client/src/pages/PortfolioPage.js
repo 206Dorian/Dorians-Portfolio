f6699e6ea53f3619b3534cb1e2a2baee0c36045e
Dorian Birch <206dorian@gmail.com>
2023-07-07 15:22:05 -0500

    updating portfolio pages and routes

--- client/src/pages/PortfolioPage.js
+++ client/src/pages/PortfolioPage.js
@@ -1,16 +1,22 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const PortfolioPage = () => {
   return (
     <>
         <h2>MY PROJECTS</h2>
         <ul>
-        <li><Link to="/projects/A">A</Link></li>
-            <br />
-            <li><Link to="/projects/B">B</Link></li>
-            <br />
-            <li><Link to="/projects/C">C</Link></li>
-        </ul>
+        <li><Link to="/projects/WeiFull">WeiFull</Link></li>
+        <br />
+        <li><Link to="/projects/WeatherDisco">WeatherDisco</Link></li>
+        <br />
+        <li><Link to="/projects/TallTalk">TallTalk</Link></li>
+        <br />
+        <li><Link to="/projects/My100">My100</Link></li>
+        <br />
+        <li><Link to="/projects/DidacticLift">DidacticLift</Link></li>
+        <br />
+        <li><Link to="/projects/ArtCart">ArtCart</Link></li>
+      </ul>
         <h2>MY RESUME</h2>
         <a href="/assets/DorianResume.pdf" target="_blank" rel="noreferrer">View my resume</a>
 
