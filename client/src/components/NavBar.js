e1b02ed68e04f555a08a9cbe3141892fb5c72512
Dorian Birch <206dorian@gmail.com>
2023-07-11 20:27:29 -0500

    debugged navbar, now styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import './NavBar.css';
 
-const NavBar = ({setCurrentPage}) => {
+const NavBar  = ({setCurrentPage}) => {
 const handlePageChange = (page) => {
   setCurrentPage(page);
 }
 
   return (
-    <div className="card-container">
+    
     <div className="card">
-      <nav>
+      <nav className="navbar">
         <button onClick={() => handlePageChange('home')}>Home</button>
-        <br />
+        
         <button onClick={() => handlePageChange('about')}>About</button>
-        <br />
+       
         <button onClick={() => handlePageChange('projects')}>Projects</button>
         {/* Add more navigation buttons if needed */}
       </nav>
     </div>
-  </div>
+  
   );
 };
 
