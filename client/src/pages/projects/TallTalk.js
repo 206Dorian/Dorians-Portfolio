374e77059bbcb29e2dde32cb6f6744fb33518d63
Dorian Birch <206dorian@gmail.com>
2023-08-04 10:46:30 -0500

    updated tall talk link to deployed heroku site

--- client/src/pages/projects/TallTalk.js
+++ client/src/pages/projects/TallTalk.js
@@ -4,7 +4,7 @@ const TallTalk = () => {
   return (
     <>
     <h3>
-    <a href="https://206dorian.github.io/Tall-Talk/" target="_blank" rel="noreferrer">
+    <a href="https://tall-talk-1dfa9fc02377.herokuapp.com/" target="_blank" rel="noreferrer">
     TallTalk
     </a>
     </h3>
