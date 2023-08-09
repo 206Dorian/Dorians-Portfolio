dfc7403e21fe34d26b286abfc6cde263af5dfa3a
Dorian Birch <206dorian@gmail.com>
2023-08-09 12:22:20 -0500

    deleted gh pages branch

--- client/src/pages/projects/WeiFull.js
+++ client/src/pages/projects/WeiFull.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './WeiFull.css'; // Import the project-specific CSS file
 
 const WeiFull = () => {
   return (
-    <>
+    <div className="project-card">
       <h3>
         <a href="https://ruxinqu.github.io/wei-full/" target="_blank" rel="noreferrer">
           Weifull
         </a>
-        </h3>
-    </>
+      </h3>
+    </div>
   );
 };
 
