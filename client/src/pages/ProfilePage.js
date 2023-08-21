2136d5f0fabc007ad55eda0e0380d3f022c77579
Dorian Birch <206dorian@gmail.com>
2023-08-21 14:41:27 -0500

    updating About Me

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './ProfilePage.css';
 import Footer from '../components/Footer';
+
+
 const Profile = () => {
     return (
         <main>
-            <h1> About Me</h1>
-            <h2 className="about-me"> Im an interior designer turned web developer, i've lived a lot of places and I call the Pacific Northwest home now. </h2>
+            
+            <h2 className="about-me"> A bit more about me and my experience
+            <img src="/assets/posed.jpg" alt="A posed picture" />
+
+            Im an interior designer turned web developer, i've lived a lot of places and I call the Pacific Northwest home now. </h2>
 
             <Footer/>
         </main>
