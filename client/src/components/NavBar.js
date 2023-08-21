ebd8a80ee797a857e8339dde70e749b9d4bba4e5
Dorian Birch <206dorian@gmail.com>
2023-08-21 14:11:15 -0500

    updating About Me

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,11 +12,12 @@ const handlePageChange = (page) => {
       <nav className="navbar">
         <button onClick={() => handlePageChange('home')}>Home</button>
         <br/><br/>
+        <button onClick={() => handlePageChange('projects')}>Want to see my Projects, right here!</button>
+        <br></br><br></br>
         <button onClick={() => handlePageChange('about')}>Want to know more about me? This is the button for you!</button>
         <br/><br/>
-        <button onClick={() => handlePageChange('projects')}>Want to see my Projects, right here!</button>
-        {/* Add more navigation buttons if needed */}
-      </nav>
+
+              </nav>
     </div>
   
   );
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ProfilePage.css';
-
+import Footer from '../components/Footer';
 const Profile = () => {
     return (
         <main>
             <h1> About Me</h1>
-            <h2 className="about-me"> Well....</h2>
+            <h2 className="about-me"> Im an interior designer turned web developer, i've lived a lot of places and I call the Pacific Northwest home now. </h2>
+
+            <Footer/>
         </main>
     ); 
 }
