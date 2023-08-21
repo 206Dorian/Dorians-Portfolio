a4c77f4e0261286574e2ef6811c2ac28586de30a
Dorian Birch <206dorian@gmail.com>
2023-08-21 15:05:59 -0500

    update dprofile pic named posed and worki g on spacing

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './ProfilePage.css';
 import Footer from '../components/Footer';
 
-
 const Profile = () => {
     return (
         <main>
-            
-            <h2 className="about-me"> A bit more about me and my experience
-            <img src="/assets/posed.jpg" alt="A posed picture" />
-
-            Im an interior designer turned web developer, i've lived a lot of places and I call the Pacific Northwest home now. </h2>
-
-            <Footer/>
+            <div className="main-content">
+                <img className="profile-image" src="/assets/posed.jpg" alt="A posed picture" />
+                
+                <div className="about-me"> 
+                    <h2>A bit more about me and my experience</h2>
+                    <p>Im an interior designer turned web developer, I've lived a lot of places and I call the Pacific Northwest home now.</p>
+                   
+                </div>
+                
+            </div>
+            <Footer />
         </main>
-    ); 
+    );
 }
 
 export default Profile;
