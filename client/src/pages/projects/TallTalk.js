c5a13fe3c73109c23f79cf9f62b3f683cc7915fa
Dorian Birch <206dorian@gmail.com>
2023-09-10 18:00:21 -0500

    updated info on each project card

--- client/src/pages/projects/TallTalk.js
+++ client/src/pages/projects/TallTalk.js
@@ -7,12 +7,11 @@ const TallTalk = () => {
       <h1>
         <a href="https://tall-talk-1dfa9fc02377.herokuapp.com/" target="_blank" rel="noreferrer">
           TallTalk
-          
-        
-      <img src={questions} alt="TallTalk project preview" />
+
+          <img src={questions} alt="TallTalk project preview" />
         </a>
 
-      <h6> Tall Talk is a random question generator to help you get to know people or get the party started! New questions diplayed daily plus a database full to help you get to know someone beyond basic small talk. </h6>
+        <h6> Tall Talk is a random question generator to help you get to know people or get the party started! New questions diplayed daily plus a database full to help you get to know someone beyond basic small talk. </h6>
 
       </h1>
     </div>
