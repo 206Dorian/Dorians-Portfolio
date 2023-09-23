0a7ba2b674642fed66e727f31acd2e819729f6e8
Dorian Birch <206dorian@gmail.com>
2023-09-23 16:27:01 -0500

    updated tall talk favicon

--- client/src/pages/projects/TallTalk.js
+++ client/src/pages/projects/TallTalk.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import questions from '../../assets/questions.jpg';
+import bubble from '../../assets/bubble.jpg'
 
 const TallTalk = () => {
   return (
@@ -8,7 +9,7 @@ const TallTalk = () => {
         <a href="https://tall-talk-1dfa9fc02377.herokuapp.com/" target="_blank" rel="noreferrer">
           TallTalk
 
-          <img src={questions} alt="TallTalk project preview" />
+          <img src={bubble} alt="TallTalk project preview" />
         </a>
 
         <h6> Tall Talk is a random question generator to help you get to know people or get the party started! New questions diplayed daily plus a database full to help you get to know someone beyond basic small talk. </h6>
