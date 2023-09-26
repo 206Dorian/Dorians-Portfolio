b44fb829b60236f4cfe5a788a13fe8bf761d171b
Dorian Birch <206dorian@gmail.com>
2023-09-26 13:19:21 -0500

    adding a dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 
 import './App.css';
+import './dark-mode.css';
+
 
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/PortfolioPage';
@@ -13,48 +15,64 @@ import TallTalk from './pages/projects/TallTalk';
 import WeatherDisco from './pages/projects/WeatherDisco';
 import WeiFull from './pages/projects/WeiFull';
 import NavBar from './components/NavBar';
-import ProfilePage from './pages/ProfilePage'
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
+    const [currentPage, setCurrentPage] = useState('home');
 
-  useEffect(() => {
-    // Simulating initialization time
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
+    const toggleDarkMode = () => {
+      setDarkMode(!darkMode);
+      if (darkMode) {
+          document.body.classList.add('dark-mode');
+      } else {
+          document.body.classList.remove('dark-mode');
+      }
+  };
 
-  const [currentPage, setCurrentPage] = useState('home');
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  }
+    useEffect(() => {
+        // Simulating initialization time
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
 
-  const renderPage = () => {
-    if (currentPage === 'home') { return <HomePage /> }
-    else if (currentPage === 'projects') { return <PortfolioPage /> }
-    else if (currentPage === 'about') { return <ProfilePage /> }
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
-  }
+    const renderPage = () => {
+        switch(currentPage) {
+            case 'home': return <HomePage />;
+            case 'projects': return <PortfolioPage />;
+            case 'about': return <ProfilePage />;
+            default: return <HomePage />;
+        }
+    };
 
-  return (
-
-    <div className="App">
-      {isLoading ? (
-        <div className="loading-container">
-          {/* <img src={LoadingImage} className="loading-image" alt="Loading" /> */}
+    return (
+        <div className="App">
+            {isLoading ? (
+                <div className="loading-container">
+                    {/* <img src={LoadingImage} className="loading-image" alt="Loading" /> */}
+                </div>
+            ) : (
+                <>
+                    
+                    <Header /> 
+                    <NavBar setCurrentPage={handlePageChange} />
+                    {renderPage()}
+                    <button 
+    onClick={toggleDarkMode} 
+    style={{ backgroundColor: darkMode ? '#000' : '#FFF', color: darkMode ? '#FFF' : '#000' }}
+>
+    Toggle Dark Mode
+</button>
+                </>
+            )}
         </div>
-      ) : (
-        <>
-          <Header />
-
-          <NavBar setCurrentPage={handlePageChange} />
-          {renderPage()}
-        </>
-      )}
-    </div>
-
-  );
+    );
 }
 
 export default App;
